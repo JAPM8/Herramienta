5eabaacfd8e1af3f400599c5578e2e97927bb450
Diego Mendez <men19673@uvg.edu.gt>
2023-09-18 18:45:08 -0600

    Activamos el permiso para que el usuario pueda insertar datos.
    No solo al usar el usuario ROOT

--- App Designer/sql_matlab_create_consolidado.m
+++ App Designer/sql_matlab_create_consolidado.m
@@ -2,13 +2,13 @@ datasource = "toolbox";
 username = "root";
 password = "2023";
 conn = database(datasource,username,password);
-% Createsconsolidado_strings = importdata('Creates consolidado.sql', ';')
-% query = strjoin(Createsconsolidado_strings);
-% query= strsplit(query, ';')
-% % load('Createsconsolidado_string.mat');
-% for i  = 1:length(query)-1 %El split crea una celda extra
-%     queryexe= strcat(query{i},';')
-%     execute(conn,queryexe);
-% end
+Createsconsolidado_strings = importdata('Creates_consolidado.sql', ';')
+query = strjoin(Createsconsolidado_strings);
+query= strsplit(query, ';')
+% load('Createsconsolidado_string.mat');
+for i  = 1:length(query)-1 %ECl split crea una celda extra
+    queryexe= strcat(query{i},';')
+    execute(conn,queryexe);    
+end
 
 
