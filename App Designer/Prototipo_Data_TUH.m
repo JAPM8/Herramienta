3d66ad94f2b6f201d41525ef1f38e1bc0bf53664
Javier A. Pérez <per20183@uvg.edu.gt>
2024-10-12 10:08:46 -0600

    Fin pruebas red BILSTM y LSTM

--- App Designer/Prototipo_Data_TUH.m
+++ App Designer/Prototipo_Data_TUH.m
@@ -1,13 +1,21 @@
-% Código realizado por: Javier Pérez
+%% Código realizado por: Javier Pérez
+ % Funcional 2024A-2024B
+ % CORRA ESTE CÓDIGO POR SECCIONES
+
+    % Este script utiliza el CORPUS tuh_eeg_seizure v2.0.3, el cual contiene
+    % estudios EEG anotados con enfoque a detección de crisis epilépticas. 
+
+    % Acceda al CORPUS: https://isip.piconepress.com/projects/nedc/data/tuh_eeg/tuh_eeg_seizure/
 %% Se carga directorio de data y se cambian extensiones a .csv
 % Esto se realiza pues el directorio originalmente trae las etiquetas
-% biclase con la extensión .csv_bi
+% biclase (SEIZ/BCKG) con la extensión .csv_bi
 
 % Ruta de acceso a datos CORPUS TUH SEIZURE
+    % Cambie la ruta a la de su computadora
 folderPath = ['C:\Users\javyp\Documents\UNIVERSIDAD\GraduationGateway' ...
     '\Tesis\Data\Datos_TUH\v2.0.3\edf'];
 
-% En caso no se esté en ese folder, se cambia el working directory a este
+% En caso MATLAB no se esté en ese folder, se cambia el working directory a este
 cd(folderPath);
 
 % Se obtienen todas las direcciones de los archivos con extensión .csv_bi
@@ -15,6 +23,8 @@ cd(folderPath);
 archivos = dir('**/*.csv_bi');
 
 % Ciclo for que permite cambiar la extensión .csv_bi a .csv
+    % Para mantener diferencia entre etiquetas multiclase (ya en .csv) y 
+    % biclase, se modifica el fin del nombre de las biclase a "_bi"
 for i = 1:numel(archivos)
     oldName = fullfile(archivos(i).folder,archivos(i).name);
     newName = fullfile(archivos(i).folder,[erase(archivos(i).name, ...
@@ -28,10 +38,11 @@ end
 clc
 
 % Ruta de acceso a datos CORPUS TUH SEIZURE
+    % Cambie la ruta a la de su computadora
 folderPath = ['C:\Users\javyp\Documents\UNIVERSIDAD\GraduationGateway' ...
                '\Tesis\Data\Datos_TUH\v2.0.3\edf'];
 
-% En caso no se esté en ese folder, se cambia el working directory a este
+% En caso MATLAB no se esté en ese folder, se cambia el working directory a este
 cd(folderPath);
 
 % Directorio de entrenamiento y de pruebas de los archivos "_bi.csv"
@@ -40,7 +51,9 @@ dirLbl_train = dir('train/**/*_bi.csv');
 dirLbl_dev = dir('dev/**/*_bi.csv');
 dirLbl_eval = dir('eval/**/*_bi.csv');
 
-% Filtrado según el tipo de montaje
+% Filtrado según el tipo de montaje (devuelve arreglo de index que cumplen con el criterio como 1)
+    % train se refiere a estudios de entrenamiento, dev se refiere a
+    % development (≡ validación) y eval se refiera a evaluación
 fltr_train_ar_a = contains({dirLbl_train.folder},'_tcp_ar_a');
 fltr_train_ar = contains({dirLbl_train.folder},'_tcp_ar') ~= fltr_train_ar_a;
 fltr_train_le = contains({dirLbl_train.folder},'_tcp_le');
@@ -53,7 +66,9 @@ fltr_eval_ar_a = contains({dirLbl_eval.folder},'_tcp_ar_a');
 fltr_eval_ar = contains({dirLbl_eval.folder},'_tcp_ar') ~= fltr_eval_ar_a;
 fltr_eval_le = contains({dirLbl_eval.folder},'_tcp_le');
 
-% Se separa el directorio según el tipo de filtrado
+% Se separan directorios con ayuda de los filtros
+    % Se obtienen rutas por separado para cada tipo de montaje y objetivo
+    % de data (entrenamiento, development y evaluación)
 dirLbl_train_ar = dirLbl_train(fltr_train_ar);
 dirLbl_train_ar_a = dirLbl_train(fltr_train_ar_a);
 dirLbl_train_le = dirLbl_train(fltr_train_le);
@@ -67,9 +82,11 @@ dirLbl_eval_ar_a = dirLbl_eval(fltr_eval_ar_a);
 dirLbl_eval_le = dirLbl_eval(fltr_eval_le); % OJO: No hay muestras en EVAL
 
 %% Extracción de un estudio
-% Prototipo para extracción de señales y sus etiquetas de un estudio
+ % Prototipo para extracción de canales de un estudio y sus etiquetas
 clc
 
+% Canales disponibles para estudios con montaje tipo AR
+    % Montajes LE o AR_A cambian los canales disponibles
 chnl_list = ["EEG FP1-REF","EEG FP2-REF","EEG F3-REF","EEG F4-REF",...
              "EEG C3-REF","EEG C4-REF","EEG P3-REF","EEG P4-REF",...
              "EEG O1-REF","EEG O2-REF","EEG F7-REF","EEG F8-REF",...
@@ -77,21 +94,40 @@ chnl_list = ["EEG FP1-REF","EEG FP2-REF","EEG F3-REF","EEG F4-REF",...
              "EEG FZ-REF","EEG CZ-REF","EEG PZ-REF","EEG A1-REF",...
              "EEG A2-REF"]';
 
-randIndex = randi(numel(dirLbl_train_ar)); % Frec (250): 4392 / indx: 4501
+% Aleatoriamente se obtiene número de estudio (EEG) a extraer
+randIndex = randi(numel(dirLbl_train_ar));
 
+% Dado que los directorios contienen las rutas a las etiquetas, no a los
+% EEG, se extrae el folder y el nombre del estudio seleccionado
 relPath = dirLbl_train_ar(randIndex).folder;
 relName = dirLbl_train_ar(randIndex).name;
 
+% Se formatea el nombre del archivo de las etiquetas para acceder al EEG
 edfName = [erase(relName,'_bi.csv'),'.edf'];
 
+% Se lee archivo de etiquetas (.csv), inicia a leer hasta la fila 5 y
+% las comas "," las traduce a cambio de columna
 lbl_data = readtable(fullfile(relPath,relName),"Delimiter",",","NumHeaderLines",5);
+
+% Se obtiene header del EEG, este contiene la información de registro
 edf_hdr = edfinfo(fullfile(relPath,edfName));
+
+% Se lee canales seleccionados en "chnl_list" de EEG
+  % Note que la función devuelve una tabla que se convierte a cell (table2cell)
+  % y luego el cell se convierte a array (cell2mat)
 edf_data = cell2mat(table2cell(edfread(fullfile(relPath,edfName),"SelectedSignals",chnl_list)));
 
+% Se obtiene frecuencia de muestreo del estudio 
 edf_Fs = max(unique(edf_hdr.NumSamples/seconds(edf_hdr.DataRecordDuration)));
-edf_samples = (edf_hdr.NumDataRecords*max(edf_hdr.NumSamples));
-% edf_t = ((0:(edf_samples-1))/edf_Fs)';
 
+% Se obtiene cantidad de muestras del estudio
+edf_samples = (edf_hdr.NumDataRecords*max(edf_hdr.NumSamples));
+
+% Se obtiene vector de tiempo del estudio
+edf_t = ((0:(edf_samples-1))/edf_Fs)';
+
+% Se obtiene montaje tipo AR, es decir, se restan los canales como indica
+% comentario al lado de cada fila
 edf_montage = [edf_data(:,1) - edf_data(:,11),...   % FP1-F7
                edf_data(:,11) - edf_data(:,13),...  % F7-T3
                edf_data(:,13) - edf_data(:,15),...  % T3-T5
@@ -115,30 +151,48 @@ edf_montage = [edf_data(:,1) - edf_data(:,11),...   % FP1-F7
                edf_data(:,6) - edf_data(:,8),...    % C4-P4
                edf_data(:,8) - edf_data(:,10)];     % P4-O2
 
-% if edf_Fs ~= 256
-%     disp("Aplicando resampling")
-%     figure(1);plot(edf_montage(:,1));hold on;
-% 
-%     [P,Q] = rat(256/edf_Fs);
+% Descomente el siguiente fragmento si desea aplicar resampling al EEG
+
+% Fs_new = 256;
+% if edf_Fs ~= Fs_new
+%     % Se obtiene fracción que permite la conversión entre Fs
+%     [P,Q] = rat(Fs_new/edf_Fs);
+%
+%     % Se aplica resampling al EEG
+%       % Verifique documentación de la función pues aplica filtro
+%       % anti-aliasing, en este caso no se modifica el default
 %     edf_montage = resample(edf_montage,P,Q);
-% 
-%     edf_Fs = 256;
+%     
+%     % Se actualizan variables del estudio
+%     edf_Fs = Fs_new;
 %     edf_samples = size(edf_montage,1);
-% 
-%     plot(edf_montage(:,1));hold off;
-%     legend('edf original','edf resampling')
 % end
 
+% Se genera vector de etiquetas lleno inicialmente de etiqueta "bckg" -> no convulsión
 etiquetas = repmat("bckg",edf_samples, 1);
+
+% ciclo que recorre todas las filas de la tabla de etiquetas
 nr = height(lbl_data);
 for lbl_idx = 1:nr
-    strt_lbl = lbl_data.start_time(lbl_idx);
-    stop_lbl = lbl_data.stop_time(lbl_idx);
-    lbl = lbl_data.label(lbl_idx);
 
+    % Se obtiene etiqueta de la fila
+    lbl = lbl_data.label(lbl_idx);
+    
+    % Si la etiqueta es "seiz" -> Convulsión
     if strcmp(lbl,'seiz')
-        % Convertir tiempo en segundos a índices de muestra
-        strt_idx = max(1, ceil(strt_lbl * edf_Fs)+1);
+
+        % Se obtiene tiempo de inicio de etiqueta (en segundos)
+        strt_lbl = lbl_data.start_time(lbl_idx);
+
+        % Se obtiene tiempo de fin de etiqueta (en segundos)
+        stop_lbl = lbl_data.stop_time(lbl_idx);
+
+        % Convertir tiempo de inicio a índice del array
+        strt_idx = ceil(strt_lbl * edf_Fs)+1;
+
+        % Convertir tiempo de fin a índice del array
+            % Note que la función "min" garantiza que no se supere la
+            % cantidad de muestras del EEG con la operación
         stop_idx = min(edf_samples, ceil(stop_lbl * edf_Fs)+1);
         
         % Asignar la etiqueta a las muestras correspondientes
@@ -146,17 +200,27 @@ for lbl_idx = 1:nr
     end
 
 end
+
+% Vector de etiquetas se convierte a vector categórico
 etiquetas = categorical(etiquetas);
 
-prueba = {edf_montage,etiquetas,edf_Fs};
-disp(prueba)
-figure(2)
-plotsigroi(signalMask(etiquetas),edf_montage(:,1))
+% Gráfico de un canal (chnl) con sus etiquetas
+    % Si se selecciona un canal que no existe trabaja con el último canal
+chnl = 1;
+if chnl > height(edf_montage)
+    warning(['El canal que ingreso supera la cantidad de canales disponibles.', ...
+             'Ingrese una cantidad menor o igual a ', num2str(height(edf_montage)), ...
+             '. Trabajando con canal ', num2str(height(edf_montage)), '.']);
+    chnl = height(edf_montage);
+end
+plotsigroi(signalMask(etiquetas),edf_montage(:,chnl))
 
-%% Datasets para red neuronal (poco eficiente por uso de memoria)
-% Prototipo para guardar en memoria cada señal con sus lbls, se concluye
-% que no es funcional debido al tamaño final del cell. Tras varios intentos
-% no es posible alcanzar el objetivo del prototipo.
+%% Lectura datasets (NO CORRA ESTA SECCIÖN, LEA ANTES LOS COMENTARIOS)
+% Prototipo para guardar en memoria cada estudio con sus etiquetas.
+% El código es funcional, pero debido al tamaño de memoria que ocupa la
+% lectura NO es posible completarse con 32 Gb de RAM.
+    % Se probó alocar memoria, aumentar el límite de memoría de MATLAB y
+    % optimizar el código. Conclusión: pase a la siguiente sección para la solución 
 clc
 
 chnl_list = ["EEG FP1-REF","EEG FP2-REF","EEG F3-REF","EEG F4-REF",...
@@ -309,15 +373,15 @@ DS_lbl_dev_ar = fileDatastore(fullfile(folderPath,"dev","**","*_ar","*_bi.csv"),
 DS_sgn_eval_ar = signalDatastore(fullfile(folderPath,"eval","**","*_ar"),"ReadFcn",@readTUHEDF,"FileExtensions",".edf");
 DS_lbl_eval_ar = fileDatastore(fullfile(folderPath,"eval","**","*_ar","*_bi.csv"),"ReadFcn",@read_lbl);
 
-w_ventana = 60*256; % 1 minuto
+w_ventana = 1*60*256; % 1 minuto
 
 DS_train_ar = combine(DS_sgn_train_ar, DS_lbl_train_ar);
 DS_train_ar = transform(DS_train_ar,@(data) getlbls(data,w_ventana,2));
-% DS_train_ar = shuffle(DS_train_ar);
+DS_train_ar = shuffle(DS_train_ar);
 
 DS_dev_ar = combine(DS_sgn_dev_ar, DS_lbl_dev_ar);
 DS_dev_ar = transform(DS_dev_ar,@(data) getlbls(data,w_ventana,2));
-% DS_dev_ar = shuffle(DS_dev_ar);
+DS_dev_ar = shuffle(DS_dev_ar);
 
 DS_eval_ar = combine(DS_sgn_eval_ar, DS_lbl_eval_ar);
 DS_eval_ar = transform(DS_eval_ar,@(data) getlbls(data,w_ventana,3));
@@ -387,6 +451,8 @@ function edf_set = getlbls(data,ventana,modo)
     sizeSet = data{1,3};
     lbls = data{1,4};
     largo = ventana;
+    Max = max(edf_montage,[],"all");
+    Min = min(edf_montage,[],"all");
 
     if modo ~= 3
         etiquetas = strings(max(sizeSet,largo), 1);
@@ -458,7 +524,8 @@ function edf_set = getlbls(data,ventana,modo)
 
         etiquetas = {etiquetas};
     end  
-
+    
+    edf_montage = cellfun(@(x) rescale(x,-1,1,"InputMin",Min,"InputMax",Max),edf_montage,'UniformOutput',false);
     edf_set = [edf_montage,etiquetas];
 end
 
@@ -491,7 +558,7 @@ tallDevSet = tall(DS_dev_ar);
 trainData = gather(tallTrainSet);
 devData = gather(tallDevSet);
 
-%% Creación de red neuronal LSTM
+%% Creación de red neuronal BILSTM
 BILSTM_eegnet = dlnetwork;
 
 layers = [sequenceInputLayer(22,"Normalization","zscore","NormalizationDimension","channel")
@@ -504,6 +571,7 @@ layers = [sequenceInputLayer(22,"Normalization","zscore","NormalizationDimension
 
 BILSTM_eegnet = addLayers(BILSTM_eegnet,layers);
 
+%% Opciones entrenamiento BILSTM
 options = trainingOptions("adam", ...
                           Plots = "training-progress", ...
                           InputDataFormats = "TCB", ...
@@ -519,25 +587,63 @@ options = trainingOptions("adam", ...
                           LearnRateDropPeriod = 13, ...
                           GradientThreshold = 2, ...
                           OutputNetwork = "best-validation", ...
-                          ExecutionEnvironment = "cpu");                    
+                          ExecutionEnvironment = "gpu");                    
 
 lossFcn = @(Y,T) crossentropy(Y,T, ...
                               Weights = [639/1222, 1084/95], ...
                               NormalizationFactor = "all-elements", ...
-                              WeightsFormat = "C");
+                              WeightsFormat = "UC");
 
-[BILSTM_eegnet, info] = trainnet(trainData(:,1),trainData(:,2),BILSTM_eegnet,lossFcn,options);
+[BILSTM_eegnet, infoBILSTM] = trainnet(trainData(:,1),trainData(:,2),BILSTM_eegnet,lossFcn,options);
 
-% Metrics = "accuracy", ...   
-% ValidationData = DS_dev_ar, ...
-% ValidationFrequency = 50, ...
+%% Creación de red neuronal LSTM
+
+LSTM_eegnet = dlnetwork;
+
+layers = [sequenceInputLayer(22)
+          lstmLayer(100,'OutputMode','sequence')
+          dropoutLayer(0.2)
+          lstmLayer(75,'OutputMode','sequence')
+          dropoutLayer(0.2)
+          fullyConnectedLayer(2)
+          softmaxLayer];
+
+LSTM_eegnet = addLayers(LSTM_eegnet,layers);
+LSTM_eegnet = initialize(LSTM_eegnet);
+%% Opciones entrenamiento LSTM
+options = trainingOptions("adam", ...
+                          Plots = "training-progress", ...
+                          InputDataFormats = "TCB", ...
+                          MiniBatchSize = 34, ...  
+                          Metrics = "fscore", ...
+                          ObjectiveMetricName = "fscore", ...
+                          Verbose = false, ....
+                          MaxEpochs = 30, ... 
+                          Shuffle = "every-epoch", ...
+                          InitialLearnRate = 0.001, ... 
+                          ValidationData = DS_dev_ar, ...
+                          ValidationFrequency = 51, ...
+                          LearnRateSchedule = "piecewise", ...
+                          LearnRateDropPeriod = 15, ...
+                          GradientThreshold = 1, ...
+                          OutputNetwork = "best-validation", ...
+                          ExecutionEnvironment = "gpu", ...
+                          PreprocessingEnvironment = "parallel");                    
+
+lossFcn = @(Y,T) crossentropy(Y,T, ...
+                              Weights = [639/1222, 1084/95], ...
+                              NormalizationFactor = "all-elements", ...
+                              WeightsFormat = "UC");
+
+[LSTM_eegnet, infoLSTM2] = trainnet(DS_train_ar,LSTM_eegnet,lossFcn,options);
+
 %% Prueba de un estudio
 
 sgn = read(DS_eval_ar);
-sgns = vertcat(sgn{:,1});
-lbls = vertcat(sgn{:,2});
+sgns = sgn{:,1};
+lbls = sgn{:,2};
 
-score = predict(BILSTM_eegnet,sgns,"InputDataFormats","TCB");
+score = predict(LSTM_eegnet,sgns,"InputDataFormats","TCB");
 Ypred = scores2label(score,{'bckg' 'seiz'});
 
 figure(1)
@@ -547,13 +653,13 @@ confusionchart([lbls(:)],[Ypred(:)],'Normalization','row-normalized');
 sgns = readall(DS_eval_ar);
 sgn = sgns(:,1);
 lbls = sgns(:,2);
-    scores = minibatchpredict(BILSTM_eegnet,sgn,"MiniBatchSize",150,"InputDataFormats","TCB");
-    YPred = scores2label(scores,{'n/a' 'bckg' 'seiz'});
-    
-    Ypred = reshape(YPred,28963,[]);
-    lbls_s = vertcat(lbls{:});
-    
-    confusionchart([lbls_s(:)],[Ypred(:)],'Normalization','row-normalized');
+scores = minibatchpredict(BILSTM_eegnet,sgn,"MiniBatchSize",10,"InputDataFormats","TCB","ExecutionEnvironment","cpu","Acceleration","auto");
+YPred = scores2label(scores,{'bckg' 'seiz'});
+%%
+Ypred = reshape(YPred,28963,[]);
+lbls_s = vertcat(lbls{:});
+
+confusionchart([lbls_s(:)],[Ypred(:)],'Normalization','row-normalized');
 
 %%
 % FP1	F7
--- App Designer/Stats_DataTUHSEIZ.m
+++ App Designer/Stats_DataTUHSEIZ.m
@@ -38,11 +38,54 @@ qty_lbls = qty_seizLbl + qty_bckgLbl;
 
 classWeights = [qty_lbls/(2*qty_bckgLbl), qty_lbls/(2*qty_seizLbl)];
 
-%%
+%% Obtención de índices para data set balanceado (no alcanzado)
+rng("twister")
+load("Stats_TUHSEIZ.mat")
 
-stats_trainweights = grpstats(train_wseiz,["SEIZ", "BCKG"],"numel","DataVars","n");
+train_wseiz = cell2table(train_wseiz,"VariableNames",["Path" "Fs" "n" "Seiz" "BCKG" "N/A"]);
+dev_wseiz = cell2table(dev_wseiz,"VariableNames",["Path" "Fs" "n" "SEIZ" "BCKG" "N/A"]);
+eval_wseiz = cell2table(eval_wseiz,"VariableNames",["Path" "Fs" "n" "SEIZ" "BCKG" "N/A"]);
 
-for i = 1:2
-    classFrequency(i) = stats_trainweights.GroupCount(i); %#ok<SAGROW>
-    classWeights(i) = sum(stats_trainweights.numel_n(:))/(2*classFrequency(i));
+eeg_train_wseiz = train_wseiz(train_wseiz.Fs == 256 & train_wseiz.Seiz == 0,:);
+eeg_train_wbckg = train_wseiz(train_wseiz.Fs == 256 & train_wseiz.BCKG == 0,:);
+
+eeg_dev_wseiz = dev_wseiz(dev_wseiz.Fs == 256 & dev_wseiz.Seiz == 1,:);
+eeg_dev_wbckg = dev_wseiz(dev_wseiz.Fs == 256 & dev_wseiz.BCKG == 1,:);
+
+% Usar para train 406 wseiz y 16 wbckg (8%), dev 102 wseiz y 26 wbckg (25%)
+rnd_idx_trainbckg = randperm(height(eeg_train_wbckg),33)';
+rnd_idx_devseiz = randperm(height(eeg_dev_wseiz),height(eeg_dev_wseiz))';
+rnd_idx_devbckg = randperm(height(eeg_dev_wbckg),26)';
+
+eeg_dtrain_wseiz = eeg_dev_wseiz(rnd_idx_devseiz(1:107,:),:);
+eeg_train = vertcat(eeg_train_wseiz,eeg_dtrain_wseiz,eeg_train_wbckg(rnd_idx_trainbckg,:));
+
+eeg_dev_seiz = eeg_dev_wseiz(rnd_idx_devseiz(108:end,:),:);
+eeg_dev = vertcat(eeg_dev_seiz,eeg_dev_wbckg(rnd_idx_devbckg,:));
+
+%% Obteniendo dataset balanceado
+
+load("QtyEtiquetasTUHSEIZ.mat")
+train_wseiz.("N/A") = [];
+qty_labels = vertcat(qty_labels{:});
+train_wseiz.BCKG = qty_labels(:,1);
+train_wseiz.Seiz = qty_labels(:,2);
+
+for i = 1:height(train_wseiz)
+    Fs = train_wseiz.Fs(i);
+    if Fs ~= 256
+        train_wseiz.n(i) = train_wseiz.n(i) * 256 / Fs;
+        train_wseiz.Fs(i) = 256;
+    end
+    train_wseiz.BCKG(i) = train_wseiz.n(i) - train_wseiz.Seiz(i);
 end
+
+eeg_train_wseiz = train_wseiz(train_wseiz.Fs == 256 & train_wseiz.Seiz > 0,:);
+eeg_train_wseiz.Duration = seconds(eeg_train_wseiz.n./eeg_train_wseiz.Fs);
+train_dur_resumen = groupcounts(eeg_train_wseiz,"Duration");
+set_train = eeg_train_wseiz(eeg_train_wseiz.n == 153856,:);
+
+eeg_val_wseiz = dev_wseiz(dev_wseiz.Fs == 256 & dev_wseiz.Seiz == 1,:);
+eeg_val_wseiz.Duration = seconds(eeg_val_wseiz.n./eeg_val_wseiz.Fs);
+val_dur_resumen = groupcounts(eeg_val_wseiz,"Duration");
+set_val = eeg_val_wseiz(eeg_val_wseiz.n == 153856,:);
