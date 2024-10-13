63c0d641542e735d2c40fb4f36d431bcc2db4608
Javier A. Pérez <per20183@uvg.edu.gt>
2024-10-13 14:24:04 -0600

    Comentarios código prototipo data tuh

--- App Designer/Prototipo_Data_TUH.m
+++ App Designer/Prototipo_Data_TUH.m
@@ -160,8 +160,8 @@ edf_montage = [edf_data(:,1) - edf_data(:,11),...   % FP1-F7
 %     [P,Q] = rat(Fs_new/edf_Fs);
 %
 %     % Se aplica resampling al EEG
-%       % Verifique documentación de la función pues aplica filtro
-%       % anti-aliasing, en este caso no se modifica el default
+%     %  Verifique documentación de la función pues aplica filtro
+%     %  anti-aliasing, en este caso no se modifica el default
 %     edf_montage = resample(edf_montage,P,Q);
 %     
 %     % Se actualizan variables del estudio
@@ -192,8 +192,8 @@ for lbl_idx = 1:nr
         strt_idx = ceil(strt_lbl * edf_Fs)+1;
 
         % Convertir tiempo de fin a índice del array
-            % Note que la función "min" garantiza que no se supere la
-            % cantidad de muestras del EEG con la operación
+        %   Note que la función "min" garantiza que no se supere la
+        %   cantidad de muestras del EEG con la operación
         stop_idx = min(edf_samples, ceil(stop_lbl * edf_Fs)+1);
         
         % Asignar la etiqueta a las muestras correspondientes
@@ -374,7 +374,8 @@ DS_eval_ar = transform(DS_eval_ar,@(data) getlbls(data,w_ventana,3));
 % DS_eval_ar = shuffle(DS_eval_ar);
 
 function edf_val = readTUHEDF(filename)
-% 
+% Función de lectura para datastores de señales al hacer read de estos
+
     % Canales disponibles para estudios con montaje tipo AR
     %   Montajes LE o AR_A cambian los canales disponibles
     chnl_list = ["EEG FP1-REF","EEG FP2-REF","EEG F3-REF","EEG F4-REF",...
@@ -426,21 +427,34 @@ function edf_val = readTUHEDF(filename)
     
     clear edf_data % Limpieza de variables ya no útiles
     
+    % Se trabaja resampling a 256 Hz porque es la Fs dominante en el dataset
     if edf_Fs ~= 256
+        % Se obtiene fracción que permite la conversión entre Fs
         [P,Q] = rat(256/edf_Fs);
+
+        % Se aplica resampling al EEG
+        %  Verifique documentación de la función pues aplica filtro
+        %  anti-aliasing, en este caso no se modifica el default
         edf_montage = resample(edf_montage,P,Q);
+
+        % Se actualizan variables del estudio
         edf_Fs = 256;
         edf_samples = size(edf_montage,1);
     end
-
+    
+    % Se retornan valores de la función como cell
     edf_val = {edf_montage,edf_Fs,edf_samples};
 end
 
 function tbl_lbl = read_lbl(filename)
-   % Detectar las opciones de importación, saltando las primeras 5 líneas
+% Función de lectura para datastores de etiquetas al hacer read de estos
+
+   % Detectar las opciones de importación, saltando las primeras 5 líneas y
+   % usando las comas "," de separador de columna
     opts = detectImportOptions(filename,"Delimiter",",","NumHeaderLines", 5);
     
     % Seleccionar solo las columnas 2 a la 4
+    %   Estas contienen tiempo inicio, tiempo fin y etiqueta respectivamente
     opts.SelectedVariableNames = opts.VariableNames(2:4);
 
     % Leer la tabla con las opciones especificadas
@@ -448,119 +462,198 @@ function tbl_lbl = read_lbl(filename)
 end
 
 function edf_set = getlbls(data,ventana,modo)
+% Función de lectura de datastore transformado, en esta se normalizan las
+% señales en el rango de [-1 1] con centro en cero, también la tabla de
+% etiquetas se convierte a vector de etiquetas con etiqueta para cada
+% muestra. La función contiene 3 (todos los modos normalizan la señal):
+%   modo = 1 -> Devuelve todos los fragmentos posibles de ancho = ventana de
+%               las señales y sus etiquetas.
+%   modo = 2 -> Devuelve una ventana aleatoria de ancho = ventana de las
+%               señales y sus etiquetas. Prioriza fragmentos con etiqueta de
+%               convulsión "seiz".
+%   modo = 3 -> Devuelve las señales y etiquetas sin cortar.
+
+    % División de parámetros de la función
     edf_montage = data{1,1};
     Fs = data{1,2};
     sizeSet = data{1,3};
     lbls = data{1,4};
     largo = ventana;
+    
+    % Se centran las señales en cero
+    %   Se obtiene la media por cada canal (columna) y se le resta a c/u
+    edf_montage = edf_montage - mean(edf_montage,2);
+
+    % Se obtiene máximo y mínimo global de las señales
     Max = max(edf_montage,[],"all");
     Min = min(edf_montage,[],"all");
-
-    if modo ~= 3
-        etiquetas = strings(max(sizeSet,largo), 1);
-    else
-        etiquetas = strings(sizeSet, 1);
-    end
     
-
+    % Se genera vector para etiquetas
+    etiquetas = strings(sizeSet, 1);
+    
+    % ciclo que recorre todas las filas de la tabla de etiquetas
     nr = height(lbls);
     for lbl_idx = 1:nr
-        strt_lbl = lbls.start_time(lbl_idx);
-        stop_lbl = lbls.stop_time(lbl_idx);
-        lbl = lbls.label(lbl_idx);
-
+       
+        % Se obtiene etiqueta de la fila
+        lbl = lbl_data.label(lbl_idx);
+        
+        % Si la etiqueta es "seiz" -> Convulsión
         if strcmp(lbl,'seiz')
-            % Convertir tiempo en segundos a índices de muestra
-            strt_idx = ceil(strt_lbl * Fs)+1;
+
+            % Se obtiene tiempo de inicio de etiqueta (en segundos)
+            strt_lbl = lbls.start_time(lbl_idx);
+
+            % Se obtiene tiempo de fin de etiqueta (en segundos)
+            stop_lbl = lbls.stop_time(lbl_idx);
+
+            % Convertir tiempo de inicio a índice del array
+            strt_idx = ceil(strt_lbl * Fs) + 1;
+            
+            % Convertir tiempo de fin a índice del array
+            %   Note que la función "min" garantiza que no se supere la
+            %   cantidad de muestras del EEG con la operación
             stop_idx = min(sizeSet, ceil(stop_lbl * Fs)+1);
 
             % Asignar la etiqueta a las muestras correspondientes
             etiquetas(strt_idx:stop_idx,1) = "seiz";
-        elseif strcmp(lbl,'bckg')
-            % Convertir tiempo en segundos a índices de muestra
-            strt_idx = ceil(strt_lbl * Fs)+1;
-            stop_idx = min(sizeSet, ceil(stop_lbl * Fs)+1);
-
-            % Asignar la etiqueta a las muestras correspondientes
-            etiquetas(strt_idx:stop_idx,1) = "bckg";
         end
     end
-            
-    % etiquetas(etiquetas == "") = "n/a";
-    % catg = {'n/a' 'bckg' 'seiz'};
+    
+    % Muestras sin etiquetas se asignan a la etiqueta no convulsión "bckg"
     etiquetas(etiquetas == "") = "bckg";
-    etiquetas = categorical(etiquetas,{'bckg' 'seiz'});
+
+    % Etiquetas se codifican en formato one-hot
+    %   Esto se realiza para tener una representación simple 
+    etiquetas = onehotencode(etiquetas,2,"ClassNames",["bckg" "seiz"]);
     
     if (sizeSet >= largo) && (modo == 1)
-        numChunks = floor(sizeSet/largo);
 
+        % Cantidad de ventanas posibles de ancho = largo
+        numChunks = floor(sizeSet/largo);
+        
+        % Se limitan las señales y etiquetas a la cantidad de ventanas posibles
         edf_montage = edf_montage(1:(numChunks*largo),:);
         etiquetas = etiquetas(1:(numChunks*largo),:);
-
+        
+        % Se corta las señales y etiquetas en las ventanas posibles
+        %   Note que repmat da la distribución de filas (largo de ventana)
         edf_montage = mat2cell(edf_montage,repmat(largo,1,numChunks));
         etiquetas = mat2cell(etiquetas,repmat(largo,1,numChunks));
+
     elseif (sizeSet >= largo) && (modo == 2)
+
+        % Si no existe etiqueta "seiz" en estudio
         if ~ismember('seiz',etiquetas)
+            % Se obtiene índice inicial aleatorio para ventana
             i_indx = randperm(max(sizeSet-largo,1),1);
         else
+
+            % Se obtiene primer índice de etiqueta "seiz"
             l_inf = find(etiquetas == 'seiz',1,"first");
+
+            % Se obtiene último índice de etiqueta "seiz"
             l_sup = find(etiquetas == 'seiz',1,"last");
+
+            % Índice inicial de ventana a partir de lím. superior
             rng = l_sup - largo;
             
+            % Si la primer etiqueta de "seiz" está antes que índice inicial
             if l_inf < rng
+                
+                % Se obtiene índice inicial aleatorio entre primer etiqueta
+                % y el índice inicial obtenido anteriormente
                 i_indx = randi([l_inf, rng],1);
+            
             else
+                
+                % Índice inicial máximo entre primer muestra e índice inicial obtenido anteriormente
                 i_indx = max(rng,1);
+
             end
+
         end
         
+        % Índice final a partir del inicial más el largo de ventana
         e_indx = i_indx + largo - 1;
+
+        % Se obtiene ventana aleatoria de señales y etiquetas
         edf_montage = {edf_montage(i_indx:e_indx,:)};
         etiquetas = {etiquetas(i_indx:e_indx,:)};
-    elseif modo == 3
+    else
+        % Cuando modo = 3 o la longitud de las señales es menor al ancho de la ventana
+        % Se devuelve señales y etiquetas sin cortar
         edf_montage = {edf_montage};
         etiquetas = {etiquetas};
-    elseif (sizeSet < largo) && (modo ~= 3) 
-        edf_montage(largo,:) = 0;
-        edf_montage = {edf_montage};
 
-        etiquetas = {etiquetas};
     end  
     
+    % Reescalamiento de cada canal de la señal en el rango de [-1 1] con mínimo y máximo global
     edf_montage = cellfun(@(x) rescale(x,-1,1,"InputMin",Min,"InputMax",Max),edf_montage,'UniformOutput',false);
+    
+    % Se devuelven señales y etiquetas procesadas
     edf_set = [edf_montage,etiquetas];
+
 end
 
-%% Prueba lectura datastore
-i = 1;
-eeg_prueba = cell(100,2);
+%% Lectura datastores
+% Debajo se incluyen 4 maneras para leer datastores, tenga en cuenta los
+% comentarios en cada método
 
-while i <= 100
-    eeg_prueba(i,:) = read(DS_train_ar);
-    i = i + 1;
-end
+
+% Lectura mediante read
+%   Tenga en cuenta que read guarda el último archivo leído, por ello cada
+%   vez que llama la función lee el siguiente archivo. Si desea reiniciar
+%   la lectura utilice "reset(nombre_datastore)"
+%   Descomentar bloque debajo si desea probar
+
+% i = 1;
+% qty = 100; % Cantidad de archivos (estudios) a leer, mayor cantidad mayor memoria ocupada
+% eeg_prueba = cell(qty,2);
+% 
+% while i <= qty
+%     eeg_prueba(i,:) = read(DS_train_ar);
+%     i = i + 1;
+% end
+
+
+% Lectura mediante readall
+%   Se encuentra comentado dado lee todos los estudios a memoria, que como
+%   ya se discutió, puede llevar a llenar la memoria RAM.
+%   Descomentar línea debajo si desea probar
 
 % eeg_set = readall(DS_train_ar);
 
-%% Generación de Minibatches
 
-mbatch_train = minibatchqueue(DS_train_ar, ...
-                              MiniBatchSize = 1, ...
-                              PartialMiniBatch = "discard", ...
-                              OutputAsDlarray=[1 0], ...
-                              MiniBatchFormat=["TCB" ""], ...
-                              OutputEnvironment=["gpu" "cpu"]);
+% Lectura mediante tall arrays y función gather
+%   Se encuentra comentado dado que lee todos los estudios a memoria,que
+%   como ya se discutió, puede llevar a llenar la memoria RAM.
+%   Se recomienda leer documentación de tall arrays: https://www.mathworks.com/help/matlab/tall-arrays.html
+%   Descomentar bloque debajo si desea probar
 
-[X,Y] = next(mbatch_train);
+% tallTrainSet = tall(DS_train_ar);
+% tallDevSet = tall(DS_dev_ar);
+% 
+% trainData = gather(tallTrainSet);
+% devData = gather(tallDevSet);
 
-%% Prueba de lectura en memoria
-tallTrainSet = tall(DS_train_ar);
-tallDevSet = tall(DS_dev_ar);
 
-trainData = gather(tallTrainSet);
-devData = gather(tallDevSet);
+% Lectura mediante minibatchqueue
+%   Esta función genera una secuencia de conjuntos para la lectura de los
+%   estudios con sus etiquetas, para leer cada conjunto se utiliza la
+%   función next.
+%   Se recomienda leer documentación: https://www.mathworks.com/help/deeplearning/ref/minibatchqueue.html
+%   Descomentar bloque debajo si desea probar
 
-%% Creación de red neuronal BILSTM
+% mbatch_train = minibatchqueue(DS_train_ar, ...
+%                               MiniBatchSize = 10, ...
+%                               OutputAsDlarray=[1 0], ...
+%                               MiniBatchFormat=["TCB" ""], ...
+%                               OutputEnvironment=["gpu" "gpu"]);
+% 
+% [X,Y] = next(mbatch_train);
+
+%% Creación de red neuronal BILSTM (aquí)
 BILSTM_eegnet = dlnetwork;
 
 layers = [sequenceInputLayer(22,"Normalization","zscore","NormalizationDimension","channel")
